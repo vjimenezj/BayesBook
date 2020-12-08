ca0e5a78543767cda961ed78808ab8380c64c7a2
vjimenezj <victor09mtc@gmail.com>
2020-12-08 15:02:13 +0100

    Final version BayesBook

--- r_scripts/main.R
+++ r_scripts/main.R
@@ -9,7 +9,7 @@ dir_local <- Sys.getenv("BAYESBOOK_PATH")
 # dir_local <- "local_directory_of_BookBayes_repository"
 
 # Loading plotting inferences functions
-source(paste0(dir_local, "/r_scripts/plotting_inferences.R"))
+source(paste0(dir_local, "/r_scripts/functions_and_plotting.R"))
 
 # Simulating and inferring for each of the models
 # Defining the data for sampling from the model prior
@@ -30,7 +30,6 @@ fit_ensemble <- stan(file=paste0(dir_local, "/simulation_models/NB_simulation.st
 # Extracting samples and true parameter values to infer with Stan and for initialization
 expression <- extract(fit_ensemble, permuted = FALSE, inc_warmup = FALSE,
                            pars = c("expression"), include = TRUE)
-#expression_real <- matrix(expression[1, 1, ], nrow = G, ncol = S, byrow = FALSE)
 expression_real <- vec_into_mat(expression)
 input_data <- list("G" = G, "S" = S, "expression" = expression_real, 
                    "design" = design, "eff_length" = eff_length)
@@ -43,11 +42,11 @@ fit <- stan(file=paste0(dir_local, "/inference_models/NB_inference.stan"),
              refresh = 400, chains = nchains, 
              control = list(max_treedepth = 10))
 
+# Plots for a comparison among the inferred parameters and its simulated values
 inference_summary <- summary(fit, probs = c(0.025, 0.975))$summary
 simu_ensemble <- extract(fit_ensemble, permuted = FALSE, inc_warmup = FALSE)
 iteration <- 1
 chain <- 1
-
 par_names_means <- c("mu_alpha", "sigma_alpha", "alpha")
 par_names_changes <- "beta"
 par_names_norm <- "log_norm_factors"
@@ -55,9 +54,7 @@ par_names <- list(par_names_means, par_names_changes, par_names_norm)
 
 dir.create(paste0(dir_local, "/plots/"))
 for (parameters in par_names) {
-  title <- paste0("Simulation_", model_name, "-Inference_", model_name,
-                  "-Samples_", S, "-", paste(parameters, collapse = ","))
-  
+  title <- paste0("Simulation_and_Inference_from_NB_model-", paste(parameters, collapse = ","))
   p <- plotting_credibility(parameters, simu_ensemble = simu_ensemble,
                             iteration = 1, chain = 1, title = title,
                             inference_summary = inference_summary)
@@ -65,17 +62,17 @@ for (parameters in par_names) {
   ggsave(paste0(dir_local, "/plots/", title, ".png"))
 }
 
-if (substr(model_name, start = 1, stop = 2) == "NB") {
-  phi_infer <- as.data.frame(extract(fit)$phi)
-  colnames(phi_infer) <- "Phi"
-  phi_title <- paste0("Simulation_", model_name, "-Inference_", model_name, "-Samples_", S, "-Phi=1")
+# Histogram for phi posterior distribution
+phi_infer <- as.data.frame(extract(fit)$phi)
+colnames(phi_infer) <- "Phi"
+phi_title <- paste0("Simulation_and_Inference_from_NB_model-Phi=1")
   
-  p <- ggplot(phi_infer, aes(Phi)) + 
-   # geom_histogram(aes(y=..density..), binwidth = 0.01) +
-    geom_density(alpha=.5, fill="#00FF00") +
-    geom_vline(xintercept = 1, color = "blue", size=1.5) +
-    theme_classic()
-  
-  p
-  ggsave(paste0(dir_local, "/plots/", phi_title, ".png"))
-}
+p <- ggplot(phi_infer, aes(Phi)) + 
+  geom_histogram(aes(y=..density..), binwidth = 0.01) +
+ # geom_density(alpha=.5, fill="#00FF00") +
+  geom_vline(xintercept = 1, color = "blue", size=1.5) +
+  theme_classic()
+p
+ggsave(paste0(dir_local, "/plots/", phi_title, ".png"))
+
+
